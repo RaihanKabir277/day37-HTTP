10ddb07e91b090e5edb73b817ceeb367cd0d02bc
RaihanKabir277 <raihank0198@gmail.com>
2025-09-15 11:13:18 +0600

    post method step - 2(graph) added here

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 import requests
 
+
+USERNAME = "raihank"
+TOKEN = "12345678iu"
 pixela_api = "https://pixe.la/v1/users"
+
 user_params = {
-    "token" : "12345678iu",
-    "username" : "raihank",
+    "token" : TOKEN,
+    "username" : USERNAME,
     "agreeTermsOfService" : "yes",
     "notMinor" : "yes"
 }
+#  ------ step 1  response api endpoint ------
+# response = requests.post(url=pixela_api, json=user_params)
+# print(response.text)
 
-response = requests.post(url=pixela_api, json=user_params)
-print(response.text)
+# ------------ step 2 graph api endpoint -----
+graph_endpoint = f"{pixela_api}/{USERNAME}/graphs"
+
+headers = {
+    "X-USER-TOKEN" : TOKEN,
+}
+
+graph_config = {
+    "id" : "graph1",
+    "name" : "Cycling Graph",
+    "unit" : "km",
+    "type" : "float",
+    "color" : "ajisai",
+}
+
+graph_response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(graph_response.text)
+
 
